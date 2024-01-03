714d1746b8a5cb8881113c60b7aa59583695b042
Kruthibharath <kruthi.bharath@gmail.com>
2024-01-03 14:30:35 +0000

    feat:added simple header component with CSS implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>This is our Client server running!!!</h1>
+      <Header />
     </>
   );
 }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <>
+      {/* <Rules/> */}
+      <h2>Gotta Catch 'Em All!</h2>
+    </>
+  );
+};
