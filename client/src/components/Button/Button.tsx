6ac41f5fd2fd05e92fe297f827a23539302d321f
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-04 13:01:53 +0000

    feat: Update Button to return type button instead of accepting any type

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,12 +1,25 @@
-interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
-  classNames: string;
+import classnames from "classnames";
+
+interface ButtonProps {
+  className?: string;
+  id?: string;
   text: string;
+  onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ type, value, name, text }) => (
-  <button type={type} value={value} name={name}>
-    {text}
-  </button>
-);
+const Button: React.FC<ButtonProps> = ({ className, id, text, onClick }) => {
+  const classNames = classnames("btn", className);
+
+  return (
+    <button
+      className={classNames}
+      id={id}
+      type="button"
+      onClick={() => onClick()}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default Button;
