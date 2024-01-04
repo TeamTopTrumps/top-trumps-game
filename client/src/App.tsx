9fc0203a32de94d64ed0cf4c8ec54f063c727e04
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-04 17:08:03 +0000

    feat: add game to useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,12 @@ import PlayerScore from "./components/PlayerScore/PlayerScore";
 import { DEFAULT_ROUNDS, DEFAULT_PLAYERS } from "./constants/constants";
 import { Header } from "./components/header/Header";
 import { initialiseGame } from "./service/game";
+import { useState } from "react";
+import { Game } from "./types/game/game.types";
 function App() {
-  const game = initialiseGame(DEFAULT_PLAYERS, DEFAULT_ROUNDS);
+  const [game, setGame] = useState<Game>(
+    initialiseGame(DEFAULT_PLAYERS, DEFAULT_ROUNDS)
+  );
   const player1 = game.players[0];
   const player2 = game.players[1];
 
