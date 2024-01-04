f08274e49cc71e52582d36ea6fba9b407cc9e79d
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-04 16:18:30 +0000

    feat: initialise a game based on number of players and number of rounds parameters

--- client/src/service/game.test.tsx
+++ client/src/service/game.test.tsx
@@ -1,17 +1,27 @@
 import { initialiseGame } from "./game";
 
 describe("Game", () => {
-  it("initialise games with 2 players with no cards and no rounds played", () => {
-    const game = initialiseGame();
+  it("initialise games with 3 players and 4 rounds", () => {
+    const game = initialiseGame(3, 4);
 
-    expect(game.players.length).toBe(2);
+    expect(game.totalRounds).toBe(4);
+    expect(game.currentRound).toBe(0);
+    expect(game.roundWinners.length).toBe(0);
 
+    expect(game.players.length).toBe(3);
     const player1 = game.players[0];
     expect(player1.id).toBe("player-1");
+    expect(player1.name).toBe("Player 1");
     expect(player1.score).toBe(0);
 
     const player2 = game.players[1];
     expect(player2.id).toBe("player-2");
+    expect(player2.name).toBe("Player 2");
     expect(player2.score).toBe(0);
+
+    const player3 = game.players[2];
+    expect(player3.id).toBe("player-3");
+    expect(player3.name).toBe("Player 3");
+    expect(player3.score).toBe(0);
   });
 });
--- client/src/service/game.ts
+++ client/src/service/game.ts
@@ -1,27 +1,26 @@
 import { Game } from "../types/game/game.types";
 import { Player } from "../types/player/player.types";
 
-export function initialiseGame(): Game {
-  const player1: Player = {
-    id: "player-1",
-    name: "player1",
-    score: 0,
-    cards: [],
-  };
+export function initialiseGame(
+  numberPlayers: number,
+  numberOfRoundsToPlay: number
+): Game {
+  const players: Player[] = [];
 
-  const player2: Player = {
-    id: "player-2",
-    name: "player2",
-    score: 0,
-    cards: [],
-  };
-
-  const players: Player[] = [player1, player2];
+  for (let i = 1; i < numberPlayers + 1; i++) {
+    const player: Player = {
+      id: `player-${i}`,
+      name: `Player ${i}`,
+      score: 0,
+      cards: [],
+    };
+    players.push(player);
+  }
 
   const game = {
     players: players,
     currentRound: 0,
-    totalRounds: 5,
+    totalRounds: numberOfRoundsToPlay,
     roundsPlayed: 0,
     roundWinners: [],
   };
