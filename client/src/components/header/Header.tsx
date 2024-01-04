9f93876872800f2b81a9a5a03132b911348664bb
Kruthibharath <kruthi.bharath@gmail.com>
2024-01-04 11:03:02 +0000

    feat:added rules component to the Header

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Rules } from "../rules/Rules";
 
 export const Header: React.FC = () => {
   return (
     <>
+      <Rules />
       <h2>Gotta Catch 'Em All!</h2>
     </>
   );
