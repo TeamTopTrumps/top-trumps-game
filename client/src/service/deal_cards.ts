a21f3df4c9c2c1060f33d9e03a7372760bec4412
Paul <paulwharin@gmail.com>
2024-01-05 15:16:07 +0000

    feat: add dealCards() to services

---
+++ client/src/service/deal_cards.ts
@@ -0,0 +1,30 @@
+function dealCards(players: number, rounds: number, pack: number[]) {
+  if (players < 1) {
+    // no players, no hands
+    return [];
+  }
+
+  const cardsRequired = Math.max(0, players * rounds);
+
+  const hands = Array.from({ length: players }).map((_) => []) as number[][];
+
+  if (cardsRequired > pack.length) {
+    // Not enough cards in pack for all players - return empty hands
+    return hands;
+  }
+
+  const shuffled = new Set<number>();
+
+  while (shuffled.size < cardsRequired) {
+    const random = Math.floor(Math.random() * pack.length);
+
+    shuffled.add(pack[random]);
+  }
+
+  return Array.from(shuffled).reduce((acc, id, i) => {
+    acc[i % players].push(id);
+    return acc;
+  }, hands);
+}
+
+export { dealCards };
