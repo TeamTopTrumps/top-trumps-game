beed89a6f4cdee2a88ee49c7de41e9b92be8e2ba
Paul <paulwharin@gmail.com>
2024-01-05 16:59:30 +0000

    feat: install tanstack react-query, add setup code to main.tsx and update App.test.tsx

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 
 describe("App", () => {
   it("test App component", () => {
-    render(<App />);
+    const queryClient = new QueryClient();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    );
 
     screen.debug();
   });
