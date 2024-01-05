4d8c482146a7af4ad7c1bb6c40efc6a77e1090fe
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-05 17:24:29 +0000

    feat: Adds a winner component and uses the determineWinner method to work out the overall winner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import "./App.scss";
 import PlayerScore from "./components/PlayerScore/PlayerScore";
 import { DEFAULT_ROUNDS, DEFAULT_PLAYERS } from "./constants/constants";
 import { Header } from "./components/header/Header";
-import { initialiseGame } from "./service/game";
+import { initialiseGame, determineGameWinner } from "./service/game";
 import { useState } from "react";
 import { Game } from "./types/game/game.types";
+import GameWinner from "./components/Winner/GameWinner";
+
 function App() {
   const [game, setGame] = useState<Game>(
     initialiseGame(DEFAULT_PLAYERS, DEFAULT_ROUNDS)
@@ -12,8 +14,11 @@ function App() {
   const player1 = game.players[0];
   const player2 = game.players[1];
 
+  const overallWinner = determineGameWinner(game);
+
   return (
     <>
+      <Header />
       <PlayerScore
         playerName={player1.name}
         playerId={player1.id}
@@ -32,7 +37,7 @@ function App() {
         totalRounds={game.totalRounds}
         roundWinners={[]}
       />
-      <Header />
+      <GameWinner player={overallWinner} />
     </>
   );
 }
---
+++ client/src/components/Winner/GameWinner.tsx
@@ -0,0 +1,19 @@
+import { Player } from "../../types/player/player.types";
+
+interface GameWinnerProps {
+  player: Player | null;
+}
+
+const GameWinner: React.FC<GameWinnerProps> = ({ player }) => {
+  return (
+    <>
+      {player && (
+        <div className="game-winner">
+          Congratulations {player.name}! You caught them all!{" "}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default GameWinner;
