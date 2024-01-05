bf0bffbfd31068fc055154be4f0cde209e450d0d
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-05 16:06:01 +0000

    style: move the test line

--- client/src/service/game.test.tsx
+++ client/src/service/game.test.tsx
@@ -6,12 +6,12 @@ describe("Game", () => {
 
     expect(game.totalRounds).toBe(4);
     expect(game.roundWinners.length).toBe(0);
+    expect(game.currentRound).toBe(0);
 
     expect(game.players.length).toBe(3);
     const player1 = game.players[0];
     expect(player1.id).toBe("player-1");
     expect(player1.name).toBe("Player 1");
-    expect(game.currentRound).toBe(0);
     expect(player1.score).toBe(0);
     expect(player1.cards.length).toBe(4);
 
