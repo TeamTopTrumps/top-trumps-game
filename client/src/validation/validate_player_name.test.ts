aa689ba88443407a5da9dfbe1eceb7c8acf651f8
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-06 15:55:34 +0000

    test: Wrap validatePlayerName tests in describe functions

--- client/src/validation/validate_player_name.test.ts
+++ client/src/validation/validate_player_name.test.ts
@@ -1,34 +1,44 @@
 import validatePlayerName from "./validate_player_name";
 
-test("Given an empty string, When the function is called, Then it should return an array with a single error message", () => {
-  expect(validatePlayerName("")).toEqual(["Please provide a player name"]);
+describe("validatePlayerName should return an error message if passed an empty string", () => {
+  test("Given an empty string, When the function is called, Then it should return an array with a single error message", () => {
+    expect(validatePlayerName("")).toEqual(["Please provide a player name"]);
+  });
 });
 
-test("Given a string containing a character other than alphanumeric or space, When the function is called, Then it should return an array with a single error message", () => {
-  const specialChars = "~!@#$%^&*()-_=+[]\\{}|;':\",./<>?";
+describe("validatePlayerName should return an error message if passed a string containing invalid characters", () => {
+  test("Given a string containing a character other than alphanumeric or space, When the function is called, Then it should return an array with a single error message", () => {
+    const specialChars = "~!@#$%^&*()-_=+[]\\{}|;':\",./<>?";
 
-  specialChars
-    .split("")
-    .forEach((char) =>
-      expect(validatePlayerName(`Player ${char} 1`)).toEqual([
-        "Names cannot contain special characters",
-      ])
-    );
+    specialChars
+      .split("")
+      .forEach((char) =>
+        expect(validatePlayerName(`Player ${char} 1`)).toEqual([
+          "Names cannot contain special characters",
+        ])
+      );
+  });
 });
 
-test("Given a string containing only spaces, When the function is called, Then it should return an array with a single error message", () => {
-  expect(validatePlayerName(" ")).toEqual(["Please provide a player name"]);
-  expect(validatePlayerName("    ")).toEqual(["Please provide a player name"]);
+describe("validatePlayerName should return an error message if passed a string containing only spaces", () => {
+  test("Given a string containing only spaces, When the function is called, Then it should return an array with a single error message", () => {
+    expect(validatePlayerName(" ")).toEqual(["Please provide a player name"]);
+    expect(validatePlayerName("    ")).toEqual([
+      "Please provide a player name",
+    ]);
+  });
 });
 
-test("Given a string containing only alphanumeric characters or space, When the function is called, Then it should return an empty array", () => {
-  expect(validatePlayerName("Player 1")).toEqual([]);
-  expect(validatePlayerName("Becca R")).toEqual([]);
-  expect(validatePlayerName("55")).toEqual([]);
-});
+describe("validatePlayerName should return an empty array if passed a string containing only valid characters", () => {
+  test("Given a string containing only alphanumeric characters or space, When the function is called, Then it should return an empty array", () => {
+    expect(validatePlayerName("Player 1")).toEqual([]);
+    expect(validatePlayerName("Becca R")).toEqual([]);
+    expect(validatePlayerName("55")).toEqual([]);
+  });
 
-test("Given a string containing alphanumeric characters surrounded by spaces, When the function is called, Then it should return an empty array", () => {
-  expect(validatePlayerName(" hello ")).toEqual([]);
-  expect(validatePlayerName("hello  ")).toEqual([]);
-  expect(validatePlayerName("    hello")).toEqual([]);
+  test("Given a string containing alphanumeric characters surrounded by spaces, When the function is called, Then it should return an empty array", () => {
+    expect(validatePlayerName(" hello ")).toEqual([]);
+    expect(validatePlayerName("hello  ")).toEqual([]);
+    expect(validatePlayerName("    hello")).toEqual([]);
+  });
 });
