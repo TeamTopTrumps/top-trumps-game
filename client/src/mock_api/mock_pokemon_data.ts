f58fd955e6d95c4a217e07f9c42ff49162577f2a
Paul <paulwharin@gmail.com>
2024-01-06 23:17:21 +0000

    feat: add mock pokemon cards and handler with fetch test

--- client/src/mock_api/mock_pokemon_data.ts
+++ client/src/mock_api/mock_pokemon_data.ts
@@ -1,3 +1,308 @@
+import { Card } from "../types/card/card.types";
+
+export const pokemon_cards: Card[] = [
+  {
+    id: 1,
+    name: "bulbasaur",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/1.png",
+    description: "",
+    type: "grass,poison",
+    stats: [
+      { name: "attack", value: 49 },
+      { name: "defense", value: 49 },
+      { name: "speed", value: 45 },
+      { name: "weight", value: 69 },
+      { name: "hp", value: 45 },
+    ],
+  },
+  {
+    id: 2,
+    name: "ivysaur",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/2.png",
+    description: "",
+    type: "grass,poison",
+    stats: [
+      { name: "attack", value: 62 },
+      { name: "defense", value: 63 },
+      { name: "speed", value: 60 },
+      { name: "weight", value: 130 },
+      { name: "hp", value: 60 },
+    ],
+  },
+  {
+    id: 3,
+    name: "venusaur",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/3.png",
+    description: "",
+    type: "grass,poison",
+    stats: [
+      { name: "attack", value: 82 },
+      { name: "defense", value: 83 },
+      { name: "speed", value: 80 },
+      { name: "weight", value: 1000 },
+      { name: "hp", value: 80 },
+    ],
+  },
+  {
+    id: 4,
+    name: "charmander",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/4.png",
+    description: "",
+    type: "fire",
+    stats: [
+      { name: "attack", value: 52 },
+      { name: "defense", value: 43 },
+      { name: "speed", value: 65 },
+      { name: "weight", value: 85 },
+      { name: "hp", value: 39 },
+    ],
+  },
+  {
+    id: 5,
+    name: "charmeleon",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/5.png",
+    description: "",
+    type: "fire",
+    stats: [
+      { name: "attack", value: 64 },
+      { name: "defense", value: 58 },
+      { name: "speed", value: 80 },
+      { name: "weight", value: 190 },
+      { name: "hp", value: 58 },
+    ],
+  },
+  {
+    id: 6,
+    name: "charizard",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/6.png",
+    description: "",
+    type: "fire,flying",
+    stats: [
+      { name: "attack", value: 84 },
+      { name: "defense", value: 78 },
+      { name: "speed", value: 100 },
+      { name: "weight", value: 905 },
+      { name: "hp", value: 78 },
+    ],
+  },
+  {
+    id: 7,
+    name: "squirtle",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/7.png",
+    description: "",
+    type: "water",
+    stats: [
+      { name: "attack", value: 48 },
+      { name: "defense", value: 65 },
+      { name: "speed", value: 43 },
+      { name: "weight", value: 90 },
+      { name: "hp", value: 44 },
+    ],
+  },
+  {
+    id: 8,
+    name: "wartortle",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/8.png",
+    description: "",
+    type: "water",
+    stats: [
+      { name: "attack", value: 63 },
+      { name: "defense", value: 80 },
+      { name: "speed", value: 58 },
+      { name: "weight", value: 225 },
+      { name: "hp", value: 59 },
+    ],
+  },
+  {
+    id: 9,
+    name: "blastoise",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/9.png",
+    description: "",
+    type: "water",
+    stats: [
+      { name: "attack", value: 83 },
+      { name: "defense", value: 100 },
+      { name: "speed", value: 78 },
+      { name: "weight", value: 855 },
+      { name: "hp", value: 79 },
+    ],
+  },
+  {
+    id: 10,
+    name: "caterpie",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/10.png",
+    description: "",
+    type: "bug",
+    stats: [
+      { name: "attack", value: 30 },
+      { name: "defense", value: 35 },
+      { name: "speed", value: 45 },
+      { name: "weight", value: 29 },
+      { name: "hp", value: 45 },
+    ],
+  },
+  {
+    id: 11,
+    name: "metapod",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/11.png",
+    description: "",
+    type: "bug",
+    stats: [
+      { name: "attack", value: 20 },
+      { name: "defense", value: 55 },
+      { name: "speed", value: 30 },
+      { name: "weight", value: 99 },
+      { name: "hp", value: 50 },
+    ],
+  },
+  {
+    id: 12,
+    name: "butterfree",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/12.png",
+    description: "",
+    type: "bug,flying",
+    stats: [
+      { name: "attack", value: 45 },
+      { name: "defense", value: 50 },
+      { name: "speed", value: 70 },
+      { name: "weight", value: 320 },
+      { name: "hp", value: 60 },
+    ],
+  },
+  {
+    id: 13,
+    name: "weedle",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/13.png",
+    description: "",
+    type: "bug,poison",
+    stats: [
+      { name: "attack", value: 35 },
+      { name: "defense", value: 30 },
+      { name: "speed", value: 50 },
+      { name: "weight", value: 32 },
+      { name: "hp", value: 40 },
+    ],
+  },
+  {
+    id: 14,
+    name: "kakuna",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/14.png",
+    description: "",
+    type: "bug,poison",
+    stats: [
+      { name: "attack", value: 25 },
+      { name: "defense", value: 50 },
+      { name: "speed", value: 35 },
+      { name: "weight", value: 100 },
+      { name: "hp", value: 45 },
+    ],
+  },
+  {
+    id: 15,
+    name: "beedrill",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/15.png",
+    description: "",
+    type: "bug,poison",
+    stats: [
+      { name: "attack", value: 90 },
+      { name: "defense", value: 40 },
+      { name: "speed", value: 75 },
+      { name: "weight", value: 295 },
+      { name: "hp", value: 65 },
+    ],
+  },
+  {
+    id: 16,
+    name: "pidgey",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/16.png",
+    description: "",
+    type: "normal,flying",
+    stats: [
+      { name: "attack", value: 45 },
+      { name: "defense", value: 40 },
+      { name: "speed", value: 56 },
+      { name: "weight", value: 18 },
+      { name: "hp", value: 40 },
+    ],
+  },
+  {
+    id: 17,
+    name: "pidgeotto",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/17.png",
+    description: "",
+    type: "normal,flying",
+    stats: [
+      { name: "attack", value: 60 },
+      { name: "defense", value: 55 },
+      { name: "speed", value: 71 },
+      { name: "weight", value: 300 },
+      { name: "hp", value: 63 },
+    ],
+  },
+  {
+    id: 18,
+    name: "pidgeot",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/18.png",
+    description: "",
+    type: "normal,flying",
+    stats: [
+      { name: "attack", value: 80 },
+      { name: "defense", value: 75 },
+      { name: "speed", value: 101 },
+      { name: "weight", value: 395 },
+      { name: "hp", value: 83 },
+    ],
+  },
+  {
+    id: 19,
+    name: "rattata",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/19.png",
+    description: "",
+    type: "normal",
+    stats: [
+      { name: "attack", value: 56 },
+      { name: "defense", value: 35 },
+      { name: "speed", value: 72 },
+      { name: "weight", value: 35 },
+      { name: "hp", value: 30 },
+    ],
+  },
+  {
+    id: 20,
+    name: "raticate",
+    imageUrl:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/20.png",
+    description: "",
+    type: "normal",
+    stats: [
+      { name: "attack", value: 81 },
+      { name: "defense", value: 60 },
+      { name: "speed", value: 97 },
+      { name: "weight", value: 185 },
+      { name: "hp", value: 55 },
+    ],
+  },
+] as const;
+
 export const pokemon_list: Array<{ id: number; name: string }> = [
   {
     id: 1,
