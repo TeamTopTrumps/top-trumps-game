f7bbdebfd4261ed4c82cb737a363ff28c7c23455
Paul <paulwharin@gmail.com>
2024-01-07 01:34:10 +0000

    refactor: skip App.test.tsx test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  it("test App component", () => {
+  it.todo("test App component", () => {
     render(<App />);
 
     screen.debug();
