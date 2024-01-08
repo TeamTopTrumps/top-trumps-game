1453aafde9862eaaea5bb622e202f38a1649c265
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-08 16:53:42 +0000

    refactor: Remove player name validation and update PlayerScore props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,13 @@ import { useState, useRef } from "react";
 import { Header } from "./components/Header/Header";
 import PlayerScore from "./components/PlayerScore/PlayerScore";
 import { Game } from "./types/game/game.types";
-import { Player, PlayerNameAndId } from "./types/player/player.types";
+import { Player } from "./types/player/player.types";
 import {
   DEFAULT_ROUNDS,
   DEFAULT_PLAYERS,
   DEFAULT_TIMEOUT,
 } from "./constants/constants";
-import { initialiseGame, getInitialPlayerNames } from "./service/game/game";
-import validatePlayerName from "./validation/validate_player_name";
+import { initialiseGame } from "./service/game/game";
 import {
   calculateRoundWinner,
   chooseRandomStat,
@@ -26,38 +25,26 @@ function App() {
   const [game, setGame] = useState<Game>(
     initialiseGame(DEFAULT_PLAYERS, DEFAULT_ROUNDS)
   );
-  const player1 = game.players[0];
-  const player2 = game.players[1];
+
   const { players, totalRounds, roundWinners } = game;
 
-  const [inputPlayerNames, setInputPlayerNames] = useState<PlayerNameAndId[]>(
-    getInitialPlayerNames(game.players)
-  );
-
-  const updatePlayerName = (id: string, value: string) => {
-    setInputPlayerNames((currentInput) => {
-      return currentInput.map((input) =>
-        input.id === id ? { id: id, name: value } : input
-      );
-    });
-
-    const errorMessages = validatePlayerName(value);
-
-    if (errorMessages.length === 0) {
-      setGame((currentGame) => {
-        const updatedPlayers = currentGame.players.map((player) => {
-          return player.id === id ? { ...player, name: value.trim() } : player;
-        });
-        return {
-          ...currentGame,
-          players: updatedPlayers,
-        };
-      });
-    }
-  };
+  const player1 = players[0];
+  const player2 = players[1];
 
   const currentRoundRef = useRef<number>(0);
   const currentPlayerRef = useRef<Player>(players[0]);
+
+  const updatePlayerName = (id: string, value: string) => {
+    setGame((currentGame) => {
+      const updatedPlayers = currentGame.players.map((player) => {
+        return player.id === id ? { ...player, name: value.trim() } : player;
+      });
+      return {
+        ...currentGame,
+        players: updatedPlayers,
+      };
+    });
+  };
 
   const showPlayerCard = (player: Player) => {
     setGame((prevGame: Game) => {
@@ -163,28 +150,18 @@ function App() {
     <>
       <Header />
       <PlayerScore
-        name={
-          inputPlayerNames.find(({ id }) => id === player1.id)?.name ??
-          player1.name
-        }
+        name={player1.name}
         id={player1.id}
         updateName={updatePlayerName}
-        validateName={validatePlayerName}
         score={player1.score}
         currentRound={currentRoundRef.current}
         totalRounds={totalRounds}
         roundWinners={roundWinners}
       />
-      <script></script>
-      Player 1 name in game object is: {player1.name}
       <PlayerScore
-        name={
-          inputPlayerNames.find(({ id }) => id === player2.id)?.name ??
-          player1.name
-        }
+        name={player1.name}
         id={player2.id}
         updateName={updatePlayerName}
-        validateName={validatePlayerName}
         score={player2.score}
         currentRound={currentRoundRef.current}
         totalRounds={totalRounds}
@@ -197,7 +174,6 @@ function App() {
       <p>Player 1 score is: {player1.score}</p>
       <p>Player 1 card is: {player1.cards[0].name}</p>
       <p>Player 1 card is shown: {player1.isCardShown ? "true" : "false"}</p>
-      Player 2 name in game object is: {player2.name}
       <p>Player 2 score is {player2.score}</p>
       <p>Player 2 card is: {player2.cards[0].name}</p>
       <p>Player 2 card is shown: {player2.isCardShown ? "true" : "false"}</p>
