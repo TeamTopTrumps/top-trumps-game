784ded6e3e3bc9e454e147ca7bb4490ccd2b87e5
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-08 12:18:39 +0000

    feat: Update validatePlayerName to limit string to 15 characters and change error messages for trailing whitespace. Add and update tests for new and updated error messages

--- client/src/validation/validate_player_name.ts
+++ client/src/validation/validate_player_name.ts
@@ -1,12 +1,25 @@
 const validatePlayerName = (value: string) => {
   const errorMessages: string[] = [];
 
-  const trimmedValue = value.trim();
-
-  if (trimmedValue.length === 0) {
+  if (value.length === 0) {
     errorMessages.push("Please provide a player name");
-  } else if (trimmedValue.match(/[^A-Za-z0-9 ]/)) {
-    errorMessages.push("Names cannot contain special characters");
+  } else if (value.length > 15) {
+    errorMessages.push("Name cannot be more than 15 characters");
+  }
+
+  const firstChar = value.slice(0, 1);
+  const lastChar = value.slice(-1);
+
+  if (firstChar.match(/\s/) && !lastChar.match(/\s/)) {
+    errorMessages.push("Name cannot start with a space");
+  } else if (!firstChar.match(/\s/) && lastChar.match(/\s/)) {
+    errorMessages.push("Name cannot end with a space");
+  } else if (firstChar.match(/\s/) && lastChar.match(/\s/)) {
+    errorMessages.push("Name cannot start or end with a space");
+  }
+
+  if (value.match(/[^A-Za-z0-9 ]/)) {
+    errorMessages.push("Name cannot contain special characters");
   }
 
   return errorMessages;
