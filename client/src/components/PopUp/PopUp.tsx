2f023753eb9f07803f38b1dc7c6e1a8377faafb1
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-09 23:46:56 +0000

    feat: Update PopUp styles and props

--- client/src/components/PopUp/PopUp.tsx
+++ client/src/components/PopUp/PopUp.tsx
@@ -4,11 +4,12 @@ import classnames from "classnames";
 interface PopUpProps {
   className?: string;
   isShown: boolean;
-  handleIsShown: (value: boolean) => void;
+  handleIsShown?: (value: boolean) => void;
+  hasClose: boolean;
   children?: React.ReactNode;
 }
 const PopUp: React.FC<PopUpProps> = (props) => {
-  const { className, isShown, handleIsShown, children } = props;
+  const { className, isShown, handleIsShown, hasClose, children } = props;
 
   const classNames = classnames("popup", className);
 
@@ -16,10 +17,15 @@ const PopUp: React.FC<PopUpProps> = (props) => {
     <>
       {isShown && (
         <div className={classNames}>
-          <div className="popup-inner">
-            <button className="close-btn" onClick={() => handleIsShown(false)}>
-              X<span className="visually-hidden">Close</span>
-            </button>
+          <div className="popup__inner">
+            {hasClose && handleIsShown && (
+              <button
+                className="popup__close-btn"
+                onClick={() => handleIsShown(false)}
+              >
+                X<span className="visually-hidden">Close</span>
+              </button>
+            )}
             {children}
           </div>
         </div>
