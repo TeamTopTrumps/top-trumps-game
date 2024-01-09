2ea2a326bc00c359f7155517c51627b92295f278
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-09 23:02:02 +0000

    feat: Update Header layout and add basic styles

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,20 +1,29 @@
+import "./Header.scss";
+
 import { useState } from "react";
 import { Rules } from "../Rules/Rules";
 import PopUp from "../PopUp/PopUp";
+import Button from "../Button/Button";
 
 export const Header: React.FC = () => {
   const [buttonPopup, setButtonPopup] = useState(false);
 
-  const updateBtnPopUp = (value: boolean) => {
-    setButtonPopup(value);
+  const updateBtnPopUp = () => {
+    setButtonPopup(!buttonPopup);
   };
   return (
-    <>
-      <button onClick={() => updateBtnPopUp(true)}> Game Rules </button>
+    <header className="header">
+      <Button
+        className={"header__rules-btn"}
+        text="Game Rules"
+        onClick={updateBtnPopUp}
+      />
       <PopUp isShown={buttonPopup} handleIsShown={updateBtnPopUp}>
         <Rules />
       </PopUp>
-      <h2>Gotta Catch 'Em All!</h2>
-    </>
+      <h2 className="header__title">
+        Pokémon Top Trumps - Gotta Catch 'Em All!
+      </h2>
+    </header>
   );
 };
