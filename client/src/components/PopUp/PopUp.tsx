2853da717c3d5d7fbe18b9c52f62a9deed3f41a9
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-09 20:20:46 +0000

    feat: Revert PopUp to use conditional rendering

--- client/src/components/PopUp/PopUp.tsx
+++ client/src/components/PopUp/PopUp.tsx
@@ -10,17 +10,21 @@ interface PopUpProps {
 const PopUp: React.FC<PopUpProps> = (props) => {
   const { className, isShown, handleIsShown, children } = props;
 
-  const classNames = classnames("popup", { "popup--open": isShown }, className);
+  const classNames = classnames("popup", className);
 
   return (
-    <div className={classNames}>
-      <div className="popup-inner">
-        <button className="close-btn" onClick={() => handleIsShown(false)}>
-          X<span className="visually-hidden">Close</span>
-        </button>
-        {children}
-      </div>
-    </div>
+    <>
+      {isShown && (
+        <div className={classNames}>
+          <div className="popup-inner">
+            <button className="close-btn" onClick={() => handleIsShown(false)}>
+              X<span className="visually-hidden">Close</span>
+            </button>
+            {children}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default PopUp;
