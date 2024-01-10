dfecc3aae8a9a471093d4efd9dd50ea619a42ab0
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-10 12:21:28 +0000

    refactor: Refactor GameWinner return

--- client/src/components/Winner/GameWinner.tsx
+++ client/src/components/Winner/GameWinner.tsx
@@ -8,17 +8,12 @@ interface GameWinnerProps {
 const GameWinner: React.FC<GameWinnerProps> = (props) => {
   const { players } = props;
 
-  const singleWinner = players.length === 1;
-  const winnerNameString = singleWinner
-    ? players[0].name
-    : players.map((p) => p.name).join(", ");
-
   return (
     <>
       {players[0] &&
-        (singleWinner ? (
+        (players.length === 1 ? (
           <div className="game-result game-result--winner">
-            <p className="game-result__text">{`Congratulations ${winnerNameString}!`}</p>
+            <p className="game-result__text">{`Congratulations ${players[0].name}!`}</p>
             <p className="game-result__text">You caught them all!</p>
           </div>
         ) : (
@@ -26,7 +21,7 @@ const GameWinner: React.FC<GameWinnerProps> = (props) => {
             <p
               data-testid="players-drawn1"
               className="game-result__text"
-            >{`${winnerNameString} have drawn!`}</p>
+            >{`${players.map((p) => p.name).join(", ")} have drawn!`}</p>
             <p data-testid="players-drawn2" className="game-result__text">
               Have another game to catch them all!
             </p>
