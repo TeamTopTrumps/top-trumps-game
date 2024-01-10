58a13a457a59a883529cdd2ba196ae32c99c858c
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-10 13:34:36 +0000

    refactor: Refactor GameWinner return

--- client/src/components/Winner/GameWinner.tsx
+++ client/src/components/Winner/GameWinner.tsx
@@ -10,23 +10,22 @@ const GameWinner: React.FC<GameWinnerProps> = (props) => {
 
   return (
     <>
-      {players[0] &&
-        (players.length === 1 ? (
-          <div className="game-result game-result--winner">
-            <p className="game-result__text">{`Congratulations ${players[0].name}!`}</p>
-            <p className="game-result__text">You caught them all!</p>
-          </div>
-        ) : (
-          <div className="game-result game-result--draw">
-            <p
-              data-testid="players-drawn1"
-              className="game-result__text"
-            >{`${players.map((p) => p.name).join(", ")} have drawn!`}</p>
-            <p data-testid="players-drawn2" className="game-result__text">
-              Have another game to catch them all!
-            </p>
-          </div>
-        ))}
+      {players.length === 1 ? (
+        <div className="game-result game-result--winner">
+          <p className="game-result__text">{`Congratulations ${players[0].name}!`}</p>
+          <p className="game-result__text">You caught them all!</p>
+        </div>
+      ) : (
+        <div className="game-result game-result--draw">
+          <p
+            data-testid="players-drawn1"
+            className="game-result__text"
+          >{`${players.map((p) => p.name).join(", ")} have drawn!`}</p>
+          <p data-testid="players-drawn2" className="game-result__text">
+            Have another game to catch them all!
+          </p>
+        </div>
+      )}
     </>
   );
 };
